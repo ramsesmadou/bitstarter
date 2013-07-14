14e5dca4b0c1ac0d7ab14f008b779b7864a91093
ramsesmadou <ramsesmadou@gmail.com>
2013-07-14 06:30:31 +0000

    updated web.js

--- web.js
+++ web.js
@@ -4,13 +4,11 @@ var app = express.createServer(express.logger());
 var indexFileBuffer = fs.readFileSync('index.html');
 var buf = new Buffer(indexFileBuffer);
 var indexString = buf.toString();
-
-
  
 app.get('/', function(request, response) {
   response.send(indexString);
 });
-2A
+
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
