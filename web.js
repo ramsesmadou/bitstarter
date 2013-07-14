dae948cde1044318b673ab31f1451acb745444e3
ramsesmadou <ramsesmadou@gmail.com>
2013-07-14 04:19:53 +0000

    updated web.js with first attempt at buffer read

--- web.js
+++ web.js
@@ -1,9 +1,10 @@
 var express = require('express');
 
 var app = express.createServer(express.logger());
-
+var indexFileBuffer = (index.html);
+var buf = new Buffer(indexFileBuffer);
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  response.send(buf.toString());
 });
 
 var port = process.env.PORT || 5000;
